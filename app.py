43479eaec054cd3f82ac478d981cb954970fe4dd
Muhammad Abdullah <126229662+abdullahimtiazyousafzai@users.noreply.github.com>
2025-12-18 00:47:00 +0500

    update

--- app.py
+++ app.py
@@ -32,3 +32,10 @@ def submit_result():
 @app.route("/health")
 def health():
     return "OK"
+
+
+
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
